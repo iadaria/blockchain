c1f6d4d4038f7359b37f0db1f557348fedc9e49b
Daria Iakimova <dahunichka@gmail.com>
2020-11-25 13:23:20 +0900

    _

--- web3/web3_infura/app.js
+++ web3/web3_infura/app.js
@@ -1,6 +1,8 @@
-require('dotenv').config();
+let Tx = require('ethereumjs-tx');
 const Web3 = require('web3');
-const web3 = new Web3('https://mainnet.infura.io/v3/bc0047816b8d47d8b4494f09a1a5a751');
+const web3 = new Web3('https://ropsten.infura.io/v3/bc0047816b8d47d8b4494f09a1a5a751');
+
+require('dotenv').config();
 
 web3.eth.getGasPrice().then((result) => {
     console.log('Gas price', web3.utils.fromWei(result, 'ether'));
@@ -23,4 +25,24 @@ console.log('private_key', process.env.PRIVATE_KEY_1);
 const privateKey1 = Buffer.from(process.env.PRIVATE_KEY_1, 'hex');
 const privateKey2 = Buffer.from(process.env.PRIVATE_KEY_2, 'hex');
 
-console.log(privateKey1);
+web3.eth.getTransactionCount(account1, (err, txCount))
+
+// Create transaction object
+const txObject = {
+    nonce:,
+    gasLimit: ,
+    gasPrice: ,
+    data: ,
+};
+
+// Sign the trnasaction
+const tx = new Tx(txObject);
+tx.sign(privateKey);
+
+const serializedTx = tx.serialize();
+const row = '0x' + serializedTx.toString('hex');
+
+// Broadcast the transaction
+web3.eth.sendSignedTransaction(raw, (err, txHash) => {
+    console.log('err:', err, 'txHash', txHash);
+})

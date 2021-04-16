74aebabd0b6e0d28656793c362a1fcc4ec47cd31
Daria <jadarya@mail.ru>
2021-04-16 19:32:04 +0900

    init test

---
+++ projects/decentragram/note.md
@@ -0,0 +1,8 @@
+
+### Truffle
+> truffle migrate --reset
+
+### Questions
+- global ethereum test web
+- 
+>git clone -b name-branch git-address
--- projects/decentragram/truffle-config.js
+++ projects/decentragram/truffle-config.js
@@ -35,18 +35,17 @@ module.exports = {
    * $ truffle test --network <network-name>
    */
 
-  networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
@@ -72,23 +71,26 @@ module.exports = {
     // network_id: 2111,   // This network is yours, in the cloud.
     // production: true    // Treats this network as if it was a public net. (default: false)
     // }
-  },
+  
 
   // Set default mocha options here, use special reporters etc.
   mocha: {
     // timeout: 100000
   },
-
+  contracts_directory: './src/contracts',
+  contracts_build_directory: './src/abis',
+  //migrations_directory: './',
   // Configure your compilers
+  workingDirectory: './src',
   compilers: {
     solc: {
       // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
+       optimizer: {
+         enabled: false,
+         runs: 200
+       },
       //  evmVersion: "byzantium"
       // }
     }

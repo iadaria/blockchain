3b0fe9a31b4cd9e95c8b893bfffb43b2b766305e
Daria <jadarya@mail.ru>
2021-10-23 13:25:35 +0900

    init

---
+++ sampleOne/block.js
@@ -0,0 +1,21 @@
+export class Block {
+  constructor(timestamp, lastHash, hash, data) {
+    this.timestamp = timestamp;
+    this.lastHash = lastHash;
+    this.hash = hash;
+    this.data = data;
+  }
+
+  static genesis() {
+    return new this("Genesis time", "------", "f1r57-h45h", []);
+  }
+
+  toString() {
+    return `Block -
+    Timestamp: ${this.timestamp}  
+    Last hash: ${this.lastHash.substring(0, 10)}
+    Hash: ${this.hash.substring(0, 10)}
+    Data: ${this.data}
+  `;
+  }
+}
---
+++ sampleOne/note.md
@@ -0,0 +1,5 @@
+Blcok
+- timestamp in  miliseconds
+- lastHash - the hash of the block before it
+- hash - based on its own data
+- the data to store

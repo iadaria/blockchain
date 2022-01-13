319639c583101bbb8cdc78e4299eddfd8d8a4f75
Daria <jadarya@mail.ru>
2022-01-13 11:21:32 +0900

    add WrongICO

--- Solidity/Learn base/RusOne/WrongICO.sol
+++ Solidity/Learn base/RusOne/WrongICO.sol
@@ -2,19 +2,26 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract MyFirstERC20Token {
+import 'hardhat/console.sol';
 
+contract WrongICO {
   // Объявляем переменную в которой будет название токена
   string public name;
 
   // Объявляем переменную в которй будет символ токена
   string public symbol;
 
+
   // Объявляем переменную в которй будет число нулей токена
   uint8 public decimals;
 
+  // Добавить переменную для хранения стоимости токена
+  uint public buyPrice;
+
   // Объявляем переменную в которой будет храниться общее число токенов
   uint256 public totalSupply;
+
+  address owner;
 
   // Объявляем маппинг для хранения балансво пользователей
   mapping (address => uint256) public balanceOf;
@@ -31,43 +38,65 @@ contract MyFirstERC20Token {
   // Функция инициации контракта
   constructor () {
 
+    owner = msg.sender;
+
     // Указать число нулей
     decimals = 8;
 
     // Объявляем общее число токенов, которое будет создано при инициалзиации
     totalSupply = 1000000 * (10 ** uint256(decimals)); // 10 в степени 8
 
+    // Указать значение стоимости токена
+    // Это не количество токенов, это стоймости нашей капейки токена
+    // Соответственно один токен будет стоить 1000 и еще 10 нулей
+    buyPrice = 1000; // 1000 wei
+
     // Отправляем все токены на баланс того, кто инициализировал создание контракта токена
     // Все токены оказываются у создателя
     balanceOf[msg.sender] = totalSupply;
 
     // Укзаываем название токена
-    name = "MyFirstERC20Coin";
+    name = "WrongICO";
 
     // Указываем символ токена
-    symbol = "MFE";
+    symbol = "WICO";
+  }
+
+      // Функция для отправки эфира на контракт
+  fallback() external payable {
+    _buy(msg.sender, msg.value);
+    // Вызов внутренней функции
   }
 
   // Внутренняя функция для перевода токена
   function _transfer(address _from, address _to, uint256 _value) internal {
+    console.log('[_transfer] from', _from);
+    console.log('[_transfer] owner', owner);
+    console.log('[_transfer] to', _to);
+    console.log('[_transfer] value', _value);
 
     // Проверка на пустой адрес
     require(_to != address(0x0), "Recipent address is required");
 
     // Проверка того, что отправителю хватает токенов для перевода
-    require(balanceOf[_from] >= _value, "The sender has insufficient funds");
+    //require(balanceOf[_from] >= _value, "The sender has insufficient funds");
+    require(balanceOf[owner] >= _value, "The sender has insufficient funds");
+    console.log('[_transfer] balance from', balanceOf[_from]);
+    console.log('[_transfer] balance owner', balanceOf[owner]);
 
     // Проверка на переполнение
     require(balanceOf[_to] + _value >= balanceOf[_to], "Stack overflow");
 
     // Токены списываются у отправителя
-    balanceOf[_from] -= _value;
+    //balanceOf[_from] -= _value;
+    balanceOf[owner] -= _value;
 
     // Токены прибавляются получателю
     balanceOf[_to] += _value;
 
     // Эвент перевода токенов
-    emit Transfer(_from, _to, _value);
+    // emit Transfer(_from, _to, _value);
+    emit Transfer(owner, _to, _value);
   }
 
 
@@ -76,6 +105,24 @@ contract MyFirstERC20Token {
 
     // Вызов внутренней функции перевода
     _transfer(msg.sender, _to, _value);
+  }
+
+    // Функция для обработки вызова
+  function buy() public payable {
+    // Вызов внутренней функции
+    _buy(msg.sender, msg.value);
+  }
+   // Внутрення функция
+  function _buy(address _from, uint _value) internal {
+    console.log('[_buy()] _from', _from);
+    console.log('[_buy()] _value', _value);
+    
+    // Рассчитать количество токенов по курсу
+    uint256 amount = _value / buyPrice;
+    console.log('[_buy()] amount', amount);
+
+    // Вызво внутрнней функции для перевода токнов
+    _transfer(address(this), _from, amount);
   }
 
   // Функция для перевода "одобренных" токенов

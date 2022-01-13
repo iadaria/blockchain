f245ddb03297aa9d5e3bddaed6d46361346eca34
Daria <jadarya@mail.ru>
2022-01-13 12:00:15 +0900

    delete

---
+++ Solidity/Learn base/RusOne/MinimalisticICO.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+// Объявляем интерфейс
+interface Token {
+    function transfer(address _reciever, uint256 _amount) external;
+}
+
+contract MyFirstSafeICo {
+
+    // Объявляем переменную для указания стоимости токена
+    uint public buyPrice;
+
+    // Объявляем переменную для хранения токена
+    Token public token; // просто адрес смарт-контракта и вызовем на этом адрес функцию
+
+    // Функция инициализации (принимаем токен)
+    constructor(Token _token) {
+        // Присваиваем токен
+        token = _token;
+
+        // Присваиваем стоимость
+        buyPrice = 10000;
+    }
+
+    // Функция для прямого перевода эфира
+    fallback() external payable {
+        // Вызов внутренней функции
+        _buy(msg.sender, msg.value);
+    }
+
+    // Функция для вызова
+    function buy() public payable returns (uint) {
+        // Вызов внутренней функции для покупки токенов - для фронт разрабов
+        uint tokens = _buy(msg.sender, msg.value);
+    
+        // Возвращаем значение
+        return tokens;
+    }
+
+    // Внутренняя функция
+    function _buy(address _sender, uint256 _amount) internal returns (uint) {
+        // Рассчитываем стоимость
+        uint tokens = _amount / buyPrice;
+
+        // Отправляем токены с помощью метода токена
+        token.transfer(_sender, tokens);
+
+        // Возвращаем значение удалось или нет
+        return tokens;
+    }
+}
--- Solidity/sampleOne/tests/note.md
+++
@@ -1,6 +0,0 @@
-> truffle init .
-
-> truffle migrate --reset
-> truffle migrate
-
-> truffle test
